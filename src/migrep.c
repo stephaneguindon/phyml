aef6d32922b09f85fb1382c5ea0465bdeba12dd1
stephaneguindon <stephane.guindon@gmail.com>
2015-07-13 08:57:15 +0200

    fixed potential bug in traj moves

--- src/migrep.c
+++ src/migrep.c
@@ -1274,11 +1274,11 @@ phydbl *MIGREP_MCMC(t_tree *tree)
       if(!strcmp(tree->mcmc->move_name[move],"migrep_scale_times"))
         MCMC_MIGREP_Scale_Times(tree);
 
-      /* if(!strcmp(tree->mcmc->move_name[move],"migrep_sim")) */
-      /*   MCMC_MIGREP_Simulate_Backward(tree); */
+      if(!strcmp(tree->mcmc->move_name[move],"migrep_sim"))
+        MCMC_MIGREP_Simulate_Backward(tree);
 
-      if(!strcmp(tree->mcmc->move_name[move],"migrep_traj"))
-        MCMC_MIGREP_Lineage_Traj(tree);
+      /* if(!strcmp(tree->mcmc->move_name[move],"migrep_traj")) */
+      /*   MCMC_MIGREP_Lineage_Traj(tree); */
 
       /* if(!strcmp(tree->mcmc->move_name[move],"kappa")) */
       /*   MCMC_Kappa(tree); */
