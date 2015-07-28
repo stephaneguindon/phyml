5a4f4742b7b7568a7e120c25cfa3fbd755cb7772
stephaneguindon <stephane.guindon@gmail.com>
2015-07-28 22:58:46 +0200

    mcmc+likelihood check

--- src/migrep.c
+++ src/migrep.c
@@ -151,7 +151,7 @@ int MIGREP_Main_Simulate(int argc, char *argv[])
   /* seed = 22776; */
   /* seed = 629; */
   /* seed = 12466; */
-  seed = 17330;
+  /* seed = 17330; */
 
   printf("\n. seed: %d",seed);
   srand(seed);
