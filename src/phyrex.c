d09babdd31750fd67001cd7c622ea40a2707f7dd
stephaneguindon <stephane.guindon@gmail.com>
2015-09-02 09:40:34 +0200

    two new moves

--- src/phyrex.c
+++ src/phyrex.c
@@ -151,7 +151,7 @@ int PHYREX_Main_Simulate(int argc, char *argv[])
   /* seed = 22776; */
   /* seed = 629; */
   /* seed = 12466; */
-  seed = 14493;
+  /* seed = 14493; */
 
   printf("\n. seed: %d",seed);
   srand(seed);
