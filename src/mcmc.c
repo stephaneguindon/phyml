afb5a4a64eb208ba5e184ba97e9ef084075f5858
stephaneguindon <stephane.guindon@gmail.com>
2015-10-11 10:55:04 +0200

    new sims

--- src/mcmc.c
+++ src/mcmc.c
@@ -4474,7 +4474,7 @@ void MCMC_Complete_MCMC(t_mcmc *mcmc, t_tree *tree)
   mcmc->move_weight[mcmc->num_move_phyrex_scale_times]           = 1.0;
   mcmc->move_weight[mcmc->num_move_phyrex_ldscape_lim]           = 0.0;
   mcmc->move_weight[mcmc->num_move_phyrex_sim]                   = 0.1;
-  mcmc->move_weight[mcmc->num_move_phyrex_traj]                  = 2.0;
+  mcmc->move_weight[mcmc->num_move_phyrex_traj]                  = 1.0;
   mcmc->move_weight[mcmc->num_move_phyrex_lbda_times]            = 0.0;
   mcmc->move_weight[mcmc->num_move_phyrex_ldsk_given_disk]       = 0.1;
   mcmc->move_weight[mcmc->num_move_phyrex_indel_disk_serial]     = 0.5;
