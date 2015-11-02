258722a53c44e969d7b97dd141f01bfab0cfc56d
stephaneguindon <stephane.guindon@gmail.com>
2015-11-02 20:42:46 +0100

    flu analysis

--- src/phyrex.c
+++ src/phyrex.c
@@ -21,8 +21,8 @@ the GNU public licence. See http://www.opensource.org for details.
 
 int PHYREX_Main(int argc, char *argv[])
 {
-  /* return(PHYREX_Main_Estimate(argc,argv)); */
-  return(PHYREX_Main_Simulate(argc,argv));
+  return(PHYREX_Main_Estimate(argc,argv));
+  /* return(PHYREX_Main_Simulate(argc,argv)); */
 
 }
 
