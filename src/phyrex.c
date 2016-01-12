87256f54a4c728dda8d755427ce5ea96a04d50a1
stephaneguindon <stephane.guindon@gmail.com>
2016-01-12 13:50:58 +0100

    back to coord id tokens of length 3 and 6

--- src/phyrex.c
+++ src/phyrex.c
@@ -333,7 +333,7 @@ t_tree *PHYREX_Simulate(int n_otu, int n_sites, phydbl width, phydbl height, int
   fflush(NULL);
 
 
-  /* PHYREX_Simulate_Backward_Core(YES,tree->disk,tree); */
+  PHYREX_Simulate_Backward_Core(YES,tree->disk,tree);
   mmod->samp_area = PHYREX_Simulate_Forward_Core(n_sites,tree);
     
   PHYREX_Ldsk_To_Tree(tree);  
@@ -1157,7 +1157,7 @@ phydbl *PHYREX_MCMC(t_tree *tree)
   adjust_len             = 1E+6;
 
   tot_samp_area = 0.0;
-  For(i,tree->mmod->samp_area->n_poly) tot_samp_area += Area_Of_Poly_Monte_Carlo(tree->mmod->samp_area->a_poly[i],tree->mmod->lim);
+  /* For(i,tree->mmod->samp_area->n_poly) tot_samp_area += Area_Of_Poly_Monte_Carlo(tree->mmod->samp_area->a_poly[i],tree->mmod->lim); */
 
 
   MCMC_Complete_MCMC(mcmc,tree);
@@ -1204,9 +1204,9 @@ phydbl *PHYREX_MCMC(t_tree *tree)
   PhyML_Fprintf(fp_stats,"\n# length of a generation: %G time units",PHYREX_Generation_Length(tree));
   PhyML_Fprintf(fp_stats,"\n# clock rate: %G subst. per time unit",tree->rates->clock_r);
   PhyML_Fprintf(fp_stats,"\n# of sampled demes: %d",n_demes);
-  For(i,tree->mmod->samp_area->n_poly) PhyML_Fprintf(fp_stats,"\n# area of deme%d: %f",
-                                                     i,
-                                                     Area_Of_Poly_Monte_Carlo(tree->mmod->samp_area->a_poly[i],tree->mmod->lim));
+  /* For(i,tree->mmod->samp_area->n_poly) PhyML_Fprintf(fp_stats,"\n# area of deme%d: %f", */
+  /*                                                    i, */
+  /*                                                    Area_Of_Poly_Monte_Carlo(tree->mmod->samp_area->a_poly[i],tree->mmod->lim)); */
  
   /* Starting parameter values */
   tree->mmod->lbda = Uni()*(0.5 - 0.2) + 0.2;
