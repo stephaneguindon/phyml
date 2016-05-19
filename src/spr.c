5f9dc5e6a9dc1baa0d1f48232125fcc1c87bb603
stephaneguindon <stephane.guindon@gmail.com>
2016-05-19 18:05:24 +0200

    made a few changes to Spr_Explore function

--- src/spr.c
+++ src/spr.c
@@ -3675,8 +3675,26 @@ void Speed_Spr_Loop(t_tree *tree)
   /*****************************/
   if(tree->mod->s_opt->print == YES && tree->io->quiet == NO) PhyML_Printf("\n\n. First round of SPR moves...\n");
   lk_old = tree->c_lnL;
-  /* tree->mod->s_opt->max_depth_path    = (int)(tree->n_otu/3); */
-  tree->mod->s_opt->max_depth_path    = 5;
+  tree->mod->s_opt->max_depth_path    = 3;
+  tree->mod->s_opt->max_delta_lnL_spr = (tree->io->datatype == NT)?(0.):(0.);
+  tree->mod->s_opt->spr_lnL           = NO;
+  tree->mod->s_opt->spr_pars          = NO;
+  tree->mod->s_opt->min_diff_lk_move  = 0.1;
+  delta_lnL                           = 1.0;
+  do
+    {
+      lk_old = tree->c_lnL;
+      Speed_Spr(tree,1.0,20,delta_lnL);
+    }
+  while(FABS(tree->c_lnL - lk_old) > delta_lnL);
+  /*****************************/
+
+  Optimiz_All_Free_Param(tree,(tree->io->quiet)?(NO):(tree->mod->s_opt->print));
+
+
+  if(tree->mod->s_opt->print == YES && tree->io->quiet == NO) PhyML_Printf("\n\n. Second round of SPR moves...\n");
+  lk_old = tree->c_lnL;
+  tree->mod->s_opt->max_depth_path    = (int)(tree->n_otu/3);
   tree->mod->s_opt->max_delta_lnL_spr = (tree->io->datatype == NT)?(5.):(0.);
   tree->mod->s_opt->spr_lnL           = NO;
   tree->mod->s_opt->spr_pars          = NO;
@@ -3691,28 +3709,6 @@ void Speed_Spr_Loop(t_tree *tree)
   /*****************************/
 
   Optimiz_All_Free_Param(tree,(tree->io->quiet)?(NO):(tree->mod->s_opt->print));
- 
-  /* if(tree->mod->s_opt->print == YES && tree->io->quiet == NO) PhyML_Printf("\n\n. Second round of SPR moves...\n"); */
-  /* lk_old = tree->c_lnL; */
-  /* tree->mod->s_opt->max_depth_path    = (int)(tree->n_otu/3); */
-  /* tree->mod->s_opt->max_delta_lnL_spr = (tree->io->datatype == NT)?(5.):(0.); */
-  /* tree->mod->s_opt->spr_lnL           = YES; */
-  /* tree->mod->s_opt->spr_pars          = NO; */
-  /* tree->mod->s_opt->min_diff_lk_move  = 0.1; */
-  /* delta_lnL                           = 1.0; */
-  /* do */
-  /*   { */
-  /*     lk_old = tree->c_lnL; */
-  /*     Speed_Spr(tree,1.0,20,delta_lnL); */
-  /*   } */
-  /* while(FABS(tree->c_lnL - lk_old) > delta_lnL); */
-  /* /\*****************************\/ */
-
-  /* Optimiz_All_Free_Param(tree,(tree->io->quiet)?(NO):(tree->mod->s_opt->print)); */
-
-
-
-
 
 
   /*****************************/
