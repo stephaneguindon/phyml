e66422563261fe02a450fbd9dd0061f8268ed6c9
stephaneguindon <stephane.guindon@gmail.com>
2016-05-23 17:45:02 +0200

    doing update of l_var in Update_PMat now

--- src/spr.c
+++ src/spr.c
@@ -3730,8 +3730,7 @@ void Speed_Spr_Loop(t_tree *tree)
   if(tree->mod->s_opt->print == YES && tree->io->quiet == NO) PhyML_Printf("\n\n. Second round of SPR moves...\n");
   lk_old = tree->c_lnL;
   tree->mod->s_opt->max_depth_path    = 15;
-  /* tree->mod->s_opt->max_depth_path    = tree->n_otu; */
-  tree->mod->s_opt->max_delta_lnL_spr = (tree->io->datatype == NT)?(0.):(0.);
+  tree->mod->s_opt->max_delta_lnL_spr = (tree->io->datatype == NT)?(5.):(0.);
   tree->mod->s_opt->spr_lnL           = YES;
   tree->mod->s_opt->spr_pars          = NO;
   tree->mod->s_opt->min_diff_lk_move  = 0.01;
