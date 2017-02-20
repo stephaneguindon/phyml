e578be79460348aca6f0270b0065859d163ae27d
stephaneguindon <stephane.guindon@gmail.com>
2017-02-20 16:26:53 +0100

    benchmarks

--- src/spr.c
+++ src/spr.c
@@ -4870,7 +4870,7 @@ void Spr_List_Of_Trees(t_tree *tree)
           Lk(NULL,tree);
           tree->best_lnL = tree->c_lnL;
           Spr(tree->c_lnL,1.0,tree);
-          tree->mod->s_opt->max_depth_path = tree->max_spr_depth;
+          /* tree->mod->s_opt->max_depth_path = tree->max_spr_depth; */
         }
       while(tree->n_improvements > 5);
 
