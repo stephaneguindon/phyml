eb0663ff487c4a7b9e250c3e31c72559ab900870
stephaneguindon <stephane.guindon@gmail.com>
2017-03-20 15:40:09 +0100

    dirty commit

--- src/spr.c
+++ src/spr.c
@@ -4784,8 +4784,8 @@ void Spr_List_Of_Trees(t_tree *tree)
   t_tree **tree_list;
   phydbl *lnL_list,best_lnL;
 
-  const unsigned int list_size_first_round  = 15;
-  const unsigned int list_size_second_round = 5;
+  const unsigned int list_size_first_round  = 20;
+  const unsigned int list_size_second_round = 10;
 
   best_lnL      = UNLIKELY;
   tree->verbose = (tree->verbose == VL0) ? VL0 : VL1;
