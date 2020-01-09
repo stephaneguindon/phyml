fe05c2ce9ee242b2d5d4ec97936dca5d1eedd2f9
Stephane Guindon <stephane.guindon@gmail.com>
2020-01-09 14:06:38 +0100

    Fixed but in indel_hits HR. Likelihood calculation made faster in PhyREX

--- src/phyrex.c
+++ src/phyrex.c
@@ -329,11 +329,11 @@ void PHYREX_XML(char *xml_filename)
                          mixt_tree->mmod->lim_up->lonlat[1]);
   
   /* Initialize parameters of migrep model */
-  mixt_tree->mmod->lbda = Uni()*(mixt_tree->mmod->max_lbda - mixt_tree->mmod->min_lbda) + mixt_tree->mmod->min_lbda;
-  mixt_tree->mmod->mu   = Uni()*(mixt_tree->mmod->max_mu - mixt_tree->mmod->min_mu) + mixt_tree->mmod->min_mu;
+  /* mixt_tree->mmod->lbda = Uni()*(mixt_tree->mmod->max_lbda - mixt_tree->mmod->min_lbda) + mixt_tree->mmod->min_lbda; */
+  /* mixt_tree->mmod->mu   = Uni()*(mixt_tree->mmod->max_mu - mixt_tree->mmod->min_mu) + mixt_tree->mmod->min_mu; */
   /* mixt_tree->mmod->rad  = Uni()*(mixt_tree->mmod->max_rad - mixt_tree->mmod->max_rad/2.) + mixt_tree->mmod->max_rad/2.; */
-  /* mixt_tree->mmod->lbda = 2.0; */
-  /* mixt_tree->mmod->mu   = 0.9; */
+  mixt_tree->mmod->lbda = 1.0;
+  mixt_tree->mmod->mu   = 0.5;
   mixt_tree->mmod->rad  = mixt_tree->mmod->max_rad/2.;
   mixt_tree->mmod->sigsq = PHYREX_Update_Sigsq(mixt_tree);
 
